16d416bc0b7f6e3c821e76cb77219c1039e8807c
pauldzy <paul@dziemiela.com>
2017-11-04 13:18:10 -0400

    bump 5

--- Functions/copy_foreign_table.sql
+++ Functions/copy_foreign_table.sql
@@ -3,33 +3,29 @@ CREATE OR REPLACE FUNCTION dz_pg.copy_foreign_table(
    ,IN  pForeignTableName  varchar
    ,IN  pMetadataSchema    varchar
    ,IN  pTargetSchema      varchar
-   ,IN  pTargetName        varchar DEFAULT NULL
+   ,IN  pTargetTableName   varchar DEFAULT NULL
    ,IN  pTargetTablespace  varchar DEFAULT NULL
 ) RETURNS BOOLEAN
 AS
 $BODY$ 
 DECLARE
-   str_sql         VARCHAR(32000);
-   str_tablespace  VARCHAR(32000);
-   str_target_name VARCHAR(32000);
-   str_select      VARCHAR(32000);
-   int_count       INTEGER;
+   str_sql              VARCHAR(32000);
+   str_select           VARCHAR(32000);
+   int_count            INTEGER;
+   ary_items            VARCHAR(32000)[];
+   
+   str_tablespace       VARCHAR(255);
+   str_oracle_owner     VARCHAR(255);
+   str_oracle_tablename VARCHAR(255);
+   str_target_schema    VARCHAR(255);
+   str_target_tablename VARCHAR(255);
    
 BEGIN
    
    ----------------------------------------------------------------------------
    -- Step 10
-   -- Drop any existing table
+   -- Check over incoming parameters
    ----------------------------------------------------------------------------
-   IF pTargetName IS NOT NULL
-   THEN
-      str_target_name := pTargetName;
-      
-   ELSE
-      str_target_name := pForeignTableName;
-      
-   END IF;
-   
    IF pTargetTablespace IS NOT NULL
    THEN
       str_tablespace := 'TABLESPACE ' || pTargetTablespace || ' ';
@@ -39,79 +35,126 @@ BEGIN
    
    END IF;
    
+   IF pTargetTableName IS NOT NULL
+   THEN
+      str_target_tablename := LOWER(pTargetTableName);
+      
+   ELSE
+      str_target_tablename := LOWER(pForeignTableName);
+      
+   END IF;
+
    ----------------------------------------------------------------------------
    -- Step 20
-   -- Check for existing Oracle resource 
+   -- Retrieve oracle source from map table
    ----------------------------------------------------------------------------
    str_sql := 'SELECT '
-           || 'COUNT(*) '
-           || 'FROM '
-           || 'information_schema.tables a '
-           || 'WHERE '
-           || '    a.table_type = ''FOREIGN TABLE'' ' 
-           || '    a.table_schema = $1 '
-           || 'AND a.table_name = $2';
-           
-   EXECUTE str_sql INTO int_count USING pForeignTableOwner,pForeignTableName;
-   
-   IF int_count <> 1
-   THEN
-      RAISE EXCEPTION 'Foreign table not found.';
-   
-   END IF;
-   
-   ----------------------------------------------------------------------------
-   -- Step 30
-   -- Drop any existing table
-   ----------------------------------------------------------------------------
-   str_sql := 'DROP TABLE IF EXISTS ' || pTargetSchema || '.' || pTargetName;
-   EXECUTE str_sql;
-   
-   ----------------------------------------------------------------------------
-   -- Step 40
-   -- Create the target table
-   ----------------------------------------------------------------------------
-   str_sql := 'CREATE TABLE ' || pTargetSchema || '.' || pTargetName || ' '
-           || 'AS SELECT * FROM ' || pForeignTableOwner || '.' || pForeignTableName || ' '
-           || 'WHERE 1 = 2 ' || str_tablespace;
-   EXECUTE str_sql;
-   
-   ----------------------------------------------------------------------------
-   -- Step 50
-   -- Look for primary key on source
-   ----------------------------------------------------------------------------
-   str_sql := 'SELECT '
-           || ' a.oracle_schema '
+           || ' a.oracle_owner '
            || ',a.oracle_tablename '
            || 'FROM '
            || pMetadataSchema || '.oracle_fdw_table_map a '
            || 'WHERE '
            || '    a.foreign_table_schema = $1 '
-           || 'AND a.foreign_table_name   = $2 ';
+           || 'AND a.foreign_table_name = $2 ';
            
    BEGIN
-      EXECUTE str_sql INTO str_oracle_schema,str_oracle_tablename
-      USING pForeignTableOwner,pForeignTableName;
+      EXECUTE str_sql INTO str_oracle_owner,str_oracle_tablename
+      USING LOWER(pForeignTableOwner),LOWER(pForeignTableName);
    
    EXCEPTION
       WHEN NO_DATA_FOUND
       THEN
-         str_oracle_schema := NULL;
-         str_oracle_tablename := NULL;
+         RAISE EXCEPTION 'Mapping entry not found in metadata map table.';
          
    END;
    
    ----------------------------------------------------------------------------
+   -- Step 30
+   -- Check for existing Oracle resource 
+   ----------------------------------------------------------------------------
+   str_sql := 'SELECT '
+           || 'COUNT(*) '
+           || 'FROM '
+           || pMetadataSchema || '.all_tables a '
+           || 'WHERE '
+           || '    a.owner = $1 '
+           || 'AND a.table_name = $2 ';
+           
+   EXECUTE str_sql INTO int_count USING str_oracle_owner,str_oracle_tablename;
+   
+   IF int_count <> 1
+   THEN
+      RAISE EXCEPTION 'Oracle table not found using existing metadata mapping.';
+   
+   END IF;
+   
+   ----------------------------------------------------------------------------
+   -- Step 40
+   -- Drop any existing table
+   ----------------------------------------------------------------------------
+   str_sql := 'DROP TABLE IF EXISTS ' || pTargetSchema || '.' || pTargetName;
+   
+   EXECUTE str_sql;
+   
+   ----------------------------------------------------------------------------
+   -- Step 50
+   -- Create the target table
+   ----------------------------------------------------------------------------
+   str_sql := 'CREATE TABLE ' || pTargetSchema || '.' || pTargetName || ' '
+           || 'AS SELECT * FROM ' || pForeignTableOwner || '.' || pForeignTableName || ' '
+           || 'WHERE 1 = 2 ' || str_tablespace;
+   
+   EXECUTE str_sql;
+   
+   ----------------------------------------------------------------------------
    -- Step 60
+   -- Collect the indexing statements for table
+   ----------------------------------------------------------------------------
+   ary_items := dz_pg.extract_indexes(
+       pForeignTableOwner := pForeignTableOwner
+      ,pForeignTableName  := pForeignTableName
+      ,pMetadataSchema    := pMetadataSchema
+      ,pTargetSchema      := pTargetSchema
+      ,pTargetTableName   := pTargetTableName
+      ,pTargetTablespace  := pTargetTablespace
+   );
+   
+   FOREACH item IN ARRAY ary_items
+   LOOP
+      EXECUTE item;
+   
+   END LOOP;
+   
+   ----------------------------------------------------------------------------
+   -- Step 70
+   -- Collect the constraint statements for table
+   ----------------------------------------------------------------------------
+   ary_items := dz_pg.extract_constraints(
+       pForeignTableOwner := pForeignTableOwner
+      ,pForeignTableName  := pForeignTableName
+      ,pMetadataSchema    := pMetadataSchema
+      ,pTargetSchema      := pTargetSchema
+      ,pTargetTableName   := pTargetTableName
+   );
+   
+   FOREACH item IN ARRAY ary_items
+   LOOP
+      EXECUTE item;
+   
+   END LOOP;
+   
+   ----------------------------------------------------------------------------
+   -- Step 80
    -- Load the target table
    ----------------------------------------------------------------------------
    str_sql := 'INSERT INTO ' || pTargetSchema || '.' || pTargetName || ' '
            || 'SELECT * FROM ' || pForeignTableOwner || '.' || pForeignTableName || ' '
            || 'WHERE 1 = 1 ';
+           
    EXECUTE str_sql;
    
    ----------------------------------------------------------------------------
-   -- Step 50
+   -- Step 90
    -- Assume success
    ----------------------------------------------------------------------------
    RETURN true;
